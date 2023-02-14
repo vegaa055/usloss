241efd8be1ece8d85ed4f02aa6e3d2695a3127f6
Anthony Vega <vegaa055@email.arizona.edu>
2023-02-13 17:39:41 -0700

    Tony's changes to initial file. Addition to zap, and fork1

--- phase1/kernel.h
+++ phase1/kernel.h
@@ -8,24 +8,33 @@ struct proc_struct {
    proc_ptr       next_proc_ptr;
    proc_ptr       child_proc_ptr;
    proc_ptr       next_sibling_ptr;
-   char           name[MAXNAME];     /* process's name */
-   char           start_arg[MAXARG]; /* args passed to process */
-   context        state;             /* current context for process */
-   short          pid;               /* process id */
+   proc_ptr       parent_ptr;
+   proc_ptr       quit_child_ptr;
+   proc_ptr       next_sibling_quit;
+   proc_ptr       who_zapped;
+   proc_ptr       next_who_zapped;
+   char           name[MAXNAME];       /* process's name */
+   char           start_arg[MAXARG];   /* args passed to process */
+   context        state;               /* current context for process */
+   short          pid;                 /* process id */
    int            priority;
-   int (* start_func) (char *);   /* function where process begins -- launch */
+   int (* start_func) (char *);        /* function where process begins -- launch */
    char          *stack;
    unsigned int   stacksize;
-   int            status;         /* READY, BLOCKED, QUIT, etc. */
+   int            status;              /* READY, BLOCKED, QUIT, etc. */
+   int            quitStatus;
+   int            startTime;
+   int            zapped;
+   int            cpuStartTime;
    /* other fields as needed... */
 };
 
 struct psr_bits {
-        unsigned int cur_mode:1;
-       unsigned int cur_int_enable:1;
-        unsigned int prev_mode:1;
-        unsigned int prev_int_enable:1;
-    unsigned int unused:28;
+   unsigned int cur_mode:1;
+   unsigned int cur_int_enable:1;
+   unsigned int prev_mode:1;
+   unsigned int prev_int_enable:1;
+   unsigned int unused:28;
 };
 
 union psr_values {
@@ -39,4 +48,13 @@ union psr_values {
 #define MAXPRIORITY 1
 #define SENTINELPID 1
 #define SENTINELPRIORITY LOWEST_PRIORITY
+
+/* Process Statuses */
+#define STATUS_READY 1
+#define STATUS_RUNNING 2
+#define STATUS_QUIT 4
+#define STATUS_EMPTY 5
+#define STATUS_BLOCKED 8
+#define STATUS_JOIN_BLOCKED 9
+#define STATUS_ZAP_BLOCKED 10
 
